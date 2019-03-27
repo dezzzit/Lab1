90fa314acb2d26cba6a4e66a699d5458bbcbb0aa
artem.khaustov <artem.khaustov@globallogic.com>
2019-03-27 18:22:54 +0200

    Init

---
+++ Lab1_Memory/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Memory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (true)
+            {
+                
+                i++;
+                var testArray = new byte[i];
+                var totalMemory = GC.GetTotalMemory(false);
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+
+    }
+}
+}
