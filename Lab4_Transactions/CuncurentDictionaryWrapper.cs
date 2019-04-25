085686158beb340706d84d0d1704d34ccceeb13c
KHAUART <KHAUART@hilti.com>
2019-04-25 19:12:17 +0300

    Add wrapper

--- Lab4_Transactions/CuncurentDictionaryWrapper.cs
+++ Lab4_Transactions/CuncurentDictionaryWrapper.cs
@@ -1,12 +1,134 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Lab4_Transactions
 {
-	class Transation
+	public class CuncurentDictionaryWrapper<K,V>
 	{
+        private static object syncRoot = new object();
+
+        private Dictionary<K,V> _set;
+		//key - thread id, value - shadow set
+		private Dictionary<int, Dictionary<K, V>> _transactionShadowSets = new Dictionary<int, Dictionary<K, V>>();
+        //key - thread id, value - action
+        private Dictionary<int, Queue<Action>> _transactionEventsLog = new Dictionary<int, Queue<Action>>();
+        public CuncurentDictionaryWrapper(Dictionary<K, V> set)
+		{
+			_set = set;
+		}
+
+		public void Add(K key, V item)
+        {
+            Dictionary<K, V> shadowSet = null;
+            if (_transactionShadowSets.TryGetValue(Thread.CurrentThread.ManagedThreadId, out shadowSet))
+            {
+                shadowSet.Add(key, item);
+                _transactionEventsLog[Thread.CurrentThread.ManagedThreadId].Enqueue(() => _set.Add(key, item));
+            }
+            else
+            {
+                _set.Add(key, item);
+            }
+		}
+
+        public void Remove(K key)
+        {
+            Dictionary<K, V> shadowSet = null;
+            if (_transactionShadowSets.TryGetValue(Thread.CurrentThread.ManagedThreadId, out shadowSet))
+            {
+                shadowSet.Remove(key);
+                _transactionEventsLog[Thread.CurrentThread.ManagedThreadId].Enqueue(() => _set.Remove(key));
+            }
+            else
+            {
+                _set.Remove(key);
+            }
+        }
+
+        public V Get(K key)
+        {
+            Dictionary<K, V> shadowSet = null;
+            if (_transactionShadowSets.TryGetValue(Thread.CurrentThread.ManagedThreadId, out shadowSet))
+            {
+                V value;
+                if (shadowSet.TryGetValue(key, out value))
+                {
+                    return value;
+                }
+                else
+                {
+                    throw new Exception("Key wasn't found");
+                }
+            }
+            else
+            {
+                return _set[key];
+            }
+            
+        }
+
+        public void Rollback()
+		{
+            if (Monitor.TryEnter(syncRoot, new TimeSpan(0, 1, 0)))
+            {
+                try
+                {
+                    _transactionShadowSets.Remove(Thread.CurrentThread.ManagedThreadId);
+                    _transactionEventsLog.Remove(Thread.CurrentThread.ManagedThreadId);
+                }
+                finally
+                {
+                    Monitor.Exit(syncRoot);
+                }
+            }
+            
+        }
+
+		public void BeginTransaction()
+		{
+            if (Monitor.TryEnter(syncRoot, new TimeSpan(0, 1, 0)))
+            {
+                try
+                {
+                    _transactionShadowSets.Add(Thread.CurrentThread.ManagedThreadId, new Dictionary<K, V>());
+                    _transactionEventsLog.Add(Thread.CurrentThread.ManagedThreadId, new Queue<Action>());
+                }
+                finally
+                {
+                    Monitor.Exit(syncRoot);
+                }
+            }
+           
+		}
+
+		public void Commit()
+        {
+            var events = _transactionEventsLog[Thread.CurrentThread.ManagedThreadId];
+
+            if (Monitor.TryEnter(syncRoot, new TimeSpan(0, 1, 0)))
+            {
+                try
+                {
+                    while (events.Count > 0)
+                    {
+                        events.Dequeue().Invoke();
+                    }
+
+                    _transactionShadowSets.Remove(Thread.CurrentThread.ManagedThreadId);
+                    _transactionEventsLog.Remove(Thread.CurrentThread.ManagedThreadId);
+                }
+                finally
+                {
+                    Monitor.Exit(syncRoot);
+                }
+            }
+            
+        }
+
 	}
 }
--- Lab4_Transactions/Program.cs
+++ Lab4_Transactions/Program.cs
@@ -10,6 +10,63 @@ namespace Lab4_Transactions
 	{
 		static void Main(string[] args)
 		{
-		}
+            var testDictionary = new Dictionary<int, string>();
+          var  wrapper =  new CuncurentDictionaryWrapper<int, String>(testDictionary);
+            //wrapper.BeginTransaction();
+            //wrapper.Add(1,"1");
+            //wrapper.Add(2,"2");
+            //var test1 = wrapper.Get(2);
+            //Console.Write(test1);
+            //wrapper.Rollback();
+            //test1 = wrapper.Get(2);
+            //Console.Write(test1);
+
+            //wrapper.Add(1, "1");
+            //wrapper.Add(2, "2");
+            //wrapper.Remove(1);
+            //wrapper.Commit();
+            //var test2 = wrapper.Get(1);
+
+            Task testTask1 = new Task(() =>
+            {
+                wrapper.Add(3, "3");
+                wrapper.Add(4, "4");
+                var test3 = wrapper.Get(2);
+                Console.Write(test3);
+                wrapper.Commit();
+            });
+
+            Task testTask2 = new Task(() =>
+            {
+                wrapper.Add(5, "5");
+                wrapper.Remove(2);
+                var test3 = wrapper.Get(2);
+                Console.Write(test3);
+                wrapper.Commit();
+            });
+
+            Task testTask3 = new Task(() =>
+            {
+                wrapper.Add(6, "5");
+                wrapper.Remove(2);
+                var test3 = wrapper.Get(2);
+                Console.Write(test3);
+                wrapper.Rollback();
+            });
+
+            Task testTask4 = new Task(() =>
+            {
+                wrapper.Add(6, "5");
+                var test3 = wrapper.Get(2);
+                Console.Write(test3);
+                wrapper.Commit();
+            });
+
+            testTask1.Start();
+            testTask2.Start();
+            testTask3.Start();
+            testTask4.Start();
+
+        }
 	}
 }
